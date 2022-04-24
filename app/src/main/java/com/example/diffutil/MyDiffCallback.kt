f0403003d54d206f47657f40ad9e46c1b96cf5e1
khs3994 <z01039948003@gmail.com>
2022-04-24 19:09:46 +0900

    ✨ :: Callback & ViewAdapter 작성

---
+++ app/src/main/java/com/example/diffutil/MyDiffCallback.kt
@@ -0,0 +1,13 @@
+package com.example.diffutil
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MyDiffCallback : DiffUtil.ItemCallback<Student>() {
+    override fun areItemsTheSame(oldItem: Student, newItem: Student): Boolean { // 두 객체를 비교
+        return  oldItem.hashCode() == newItem.hashCode()
+    }
+
+    override fun areContentsTheSame(oldItem: Student, newItem: Student): Boolean { // 내용이 같은지 비교
+        return oldItem == newItem
+    }
+}
